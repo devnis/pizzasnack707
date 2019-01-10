6290790ff4e8ac8c12f95a6b83f098d319d452ce
lapiscine <denis.machado@lapiscine.pro>
2019-01-10 21:11:33 +0100

    Create page A Propos

---
+++ src/AppBundle/Controller/AProposController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lapiscine
+ * Date: 2019-01-10
+ * Time: 21:05
+ */
+
+namespace AppBundle\Controller;
+
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AProposController extends Controller
+{
+    /**
+     * @Route("homepage/aPropos", name="a_propos")
+     */
+    public function aProposAction(){
+
+    return $this->render('@App/User/aPropos.html.twig');
+    }
+}
