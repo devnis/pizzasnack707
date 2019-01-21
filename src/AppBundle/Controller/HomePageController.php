ee0e4bd5afab14dcc1b374dc7788cd5479b95a66
lapiscine <denis.machado@lapiscine.pro>
2019-01-21 23:34:28 +0100

    MVP Done

--- src/AppBundle/Controller/HomePageController.php
+++ src/AppBundle/Controller/HomePageController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends Controller
 {
     /**
-     * @Route("/home", name="home_page")
+     * @Route("/", name="home_page")
      */
     public function HomePageAction()
     {
